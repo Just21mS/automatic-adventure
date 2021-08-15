bc315ca04b8e98f5bf52634562cf62bcef7a475b
Just21mS <88966037+Just21mS@users.noreply.github.com>
2021-08-15 11:45:24 +0200

    Create just21m$-r7shq.com0.js

---
+++ r7shq.net/just21m$-r7shq.com0.js
@@ -0,0 +1,29 @@
+$(`<img src="https://r7shq.com/sico/1628480234674.gif" 
+class="" style="width:100%;margin-top: 0px;">
+<div id="des333"><a id="des444" href="https://r7shoq.com/contact.html" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, 2, 155); width: 24%; margin: 2px;" class="btn-des">توأصل معنا</a>
+<a id="des444" href="https://r7shq.net" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -96, 155); width: 24%; margin: 2px;" class="btn-des">أختصارات</a>
+<a id="des444" href="https://r7shoq.com/sub.html" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -195, 155); width: 24%; margin: 2px;" class="btn-des">اشتراكات</a>
+<a id="des444" href="https://www.r7shoq.com/rules.html" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -293, 155); width: 24%; margin: 2px;" class="btn-des">القوانين</a>
+<a id="des444" href="http://qa.r7shoq.com/" target="_blank" type="button" style="border-radius: 0px 0px 70px 70px;transform: matrix(1, 0, 0, 1, -256, 126); width: 24%; margin: 2px;" class="btn-des">أسألنيُ</a>
+<a id="des444" href="https://www.r7shoq.com/za/" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -256, 12); width: 24%; margin: 2px;" class="btn-des">الزخرفه</a>
+</div>
+`).insertBefore('.nav-tabs');
+$.getScript("https://raw.githack.com/waleed-qaid/des/master/www.1des.net-chatt-r7shq.com0.js");
+$(`<style>
+.uzr.fl.corner.borderg{margin-bottom:0!important}button.ssss.btn.fr.btn-warning.fa.fa-paint-brush{display:none}.hand.nosel.fl.uzr.border{border-radius:5px!important}textarea.form-control.borderg.primaryborder.corner.fr.ulikeins{font-size:15px!important;color:#060000;padding:4px!important}textarea.form-control.borderg.primaryborder.corner.fr.u-topic{font-size:15px!important;color:#060000;padding:4px!important}#des0{background-color:#434343}div#tlogins ::-webkit-scrollbar{width:0;height:10px}div#tlogins ::-webkit-scrollbar-button{width:5px;height:5px}div#tlogins ::-webkit-scrollbar-corner{background:0 0}div#tlogins ::-webkit-scrollbar-thumb{background:#791836;border:71px #fff;border-radius:31px;background-image:-webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important}div#tlogins ::-webkit-scrollbar-track{background:#fff;border:65px #fff;border-radius:34px}
+</style>
+`).insertBefore('body');
+$(`<img style="height:50px;width: 100%;margin-right: 3px;background-color: #ececec;" src="sico/1628480312370.gif" border="0">`).insertBefore('div#d2bc');
+$(` <center><div width="99%" id="asim7"><font style="background-color: #E0E0E0;border-radius: 14px 14px 15px 15px;font-family: 'jazeera',FontAwesome white;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > يسمح السوالف يوم الخميس فقط   </font></div><center>`).insertBefore('div#d2bc');
+$('.break.light').css({"background-image":"url(sico/1628480077465.png)"});
+$ ( ` <center> <div style =" width: 100&#1642;؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">_- ¦¦ - حيآإكم فـَي شآإٌت رحيق الشوق للجوال - ¦¦ -_❥</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$(`<a href="https://www.r7shoq.com/rules.html" target="_blank" id="as0" class="border">القوانين</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+$(`<a href="https://www.r7shoq.com/vb" target="_blank" id="as0" class="border">المنتدى</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+$(`<a href="https://www.youtube.com/channel/UC_RaK467_dKrSKInnE-0g_Q" target="_blank" id="as0" class="border">اليوتيوب</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+$(`<a href="http://www.r7shq.net/" target="_blank" id="as0" class="border">الموقع الجديد</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+
+
+
+$(".fa-gear").text("الضبط").css("width","");
+$(`<div><marquee direction="right" width="120%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> اهلا وسهلا بكم في شات <font color="#595959" class="des2"> رحيق الشوق </font>افضل حضور <font class="des2" color="#595959"> قريباً </font>مبدع الشات <font class="des2" color="#595959"> قريباً </font>افضل روم<font class="des2" color="#595959">قريبا </font>مبدع الحائط <font class="des2" color="#595959"> قريباً </font>مع تمنياتنا للجميع </marquee></div>`).insertBefore('.lonline.light.break');
+$(`<div><marquee direction="right" width="120%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> </marquee></div>`).insertBefore('.lonline.light.break');
