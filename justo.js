1f42f30a586cf04252630a46417fad31c966e25e
just21m$ <88966037+Just21mS@users.noreply.github.com>
2022-01-27 01:24:56 +0100

    justo.js

---
+++ justo.js
@@ -0,0 +1,32 @@
+$(`<a id="عاصم" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a><img id="asim70"src="sico/1602857219172.png"style="width:100%"><a id="asim27"target="_blank"type="a"rel="nofollow"title="ساكتلكم مو ادب قلة مجال"href="dookmobile.com"class="label-primary"style="margin:-2px 0 0 -374px"></a></div></div><center>`).insertBefore(".nav-tabs"),$(".nav-tabs").addClass("fl").css("width","100%"),
+$(`<center><div id="asim1">
+<a id="asim7"rel="dofollow" title="القوانين" href="https://iraqna.net/rules.html"style="border-radius: 0 4px 0 0; border-right: 2px solid#f7d322;"class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="الاشتراكات" href="https://iraqna.net/sub.html"style="border-right: 1px solid#fff;"class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="راديو" href="https://iraqna.net/radioiraqna.html"style="border-right: 1px solid#fff;"class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="زخرفة اسماء" href="https://blog.iraqna.net/z"style="border-right: 1px solid#fff;"class="fl mini  fa fa-fire btn btn-primary" target="_blank"></a><a id="asim7"rel="dofollow" title="اتصل بنا" href="https://iraqna.net/contact.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-envelope btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="قوانين السوابر" href="https://iraqna.net/ruless.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="تطبيق الشات" href="https://iraqna.net/mobile.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-android btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اختصارات الشات" href="https://iraqna.net/Shortcut.html"style="border-radius: 6px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a>
+</div></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<link rel="stylesheet" href="https://iraqna.net/IQ/style.css">
+<style>.fr.rating-box { display: inline-block !important; margin: 0px 141px 0 0 !important; }a#asim02 { width: 34px!important; height: 35px; background-image: url(https://blog.iraqna.net/chat/telegram.svg); background-repeat: no-repeat; border: 1px solid #ff000000; border-style: solid solid solid groove; background-color: #fff0!important; background-size: cover; border-right: 1px solid#fff0 !important; margin: -36px 0 0 0; }.asim01 { color: #e01818!important; background-color: #ddffff!important; border-color: #2196F3!important; border-left: 6px solid #ccc!important; text-align: end; font-size: 18px !important; margin: -5px 0 -10px 0px; height: 37px; padding: 4px; }label.ae.fa.label.fa-users.btnClAlGr.label-primary { border-radius: 0 3px 3px 0; border-right: 4px solid; border-right-style: ridge; }iframe#smoke1 { float: left; width: 26px; height: 26px; position: absolute; left: 67px; } img#smoke3 { padding: 1px 0 0; width: 54px; margin-top: 0; margin-left: -17px; margin-right: -8px; } span.busers.minix.badge.border,span.s1.fa.fa-user.label.badgex.label-as-badge.label-success,a#asim27{ background-color: hsla(0, 0%, 100%, 0) !important; border: hsla(0, 0%, 100%, 0) !important; }div#tlogins .fl.mini.u-msg {
+    font-family: 'jazeera',FontAwesome;color: #188295 !important;
+}
+span.number{display:none!important}.fa-microphone:before{font-size:24px}.broadcasters>div>.prod{float:right;width:30px;height:30px;border:#1391a7;margin:1px 11px 0 7px;text-align:center;border-radius:23%;color:#fff;background-color:#1391a7;border-left:2px solid #f7d322;border-right:2px solid #f7d322}.broadcasters>div>.prod>hr{border-top-color:#fff}.room.borderg.hand.nosel.fl .fa-microphone:before{display: none; }
+.broadcasters>div>.prod span.corner.u-topic.dots { display: none; }
+.fa-volume-off:before { content: "كتم"; font-size: 15px; } .fa-remove:before, .fa-close:before, .fa-times:before { content: "خروج"; font-size: 15px; }.evant .fa-volume-up:before {content: "استماع";margin-left: -8px; font-size: 14px; }
+</style>`).insertBefore("body");
+$('.ae.label.label-primary.fa.fa-gear').html(`⠀`);
+$('label.ae.fa.label.label-primary.fa-commenting-o').html(`⠀`);
+$('.ae.fa.label.label-primary.fa-users').html(`⠀`);
+$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"}),$(".checkbox label").html(`<input id="stealth"type="checkbox"value=""><i class="fa fa-eye-slash"aria-hidden="true"style="margin-top: 3px"></i>`),
+$(`<a href="" target="_blank" id="as0" class="border">تطبيق الشات</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+<iframe frameborder="0" id="asim222" src="https://blog.iraqna.net/coolnames/index/index/www.chatiraqiaa.com/index.html" style="height:280px"></iframe></div>`).appendTo("#grubesAll")
+clearInterval(myVar);}else{console.log(usmsgw)}},2000);})
+
+
+$(`<style>.hand.corner{background-color:#000!important;border:2px solid #c12e2a!important;color:#f0ad4e;border-radius:0;text-align:center;border-style:solid;border-right:7px solid!important;border-left:7px solid!important;border-color:#2ea1ad;border-left-style:double!important;border-right-style:double!important}div.break.fl::before{content:" اخي الزائر اختي الزائرة اذا واجهت مشكله في دخول الشات يرجى مراسلتنا من صفحة اتصل بنا ✉️ .. ";color:#0f0!important;font-family:'jazeera',FontAwesome;font-size:16px}.break.fl{text-align:center;padding:5px!important;font-family:'jazeera',FontAwesome;color:red;-webkit-animation:mymove .5s infinite;animation:mymove .5s infinite}</style>`).insertBefore("div#tlogins .lonline.light.break");
+$('.tc span[class="fa fl"]').addClass('fa-check');var nopm=!0,nonot=!0;
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<iframe id="smoke1"src="https://blog.iraqna.net/Radio" width="26px" height="26px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe>`).insertAfter('button.fa.fa-sign-out.fl.btn.btn-primary'),clearInterval(b)):console.log(c)},1)}),$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)});
+$(`<img tabindex="0" class="smoke fl"data-original-title="" title=""id="smoke3">
+`).insertAfter('img.fl.nosel.emobox');
