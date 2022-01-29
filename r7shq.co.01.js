08535dbb94498f148a77f6ddbed22459376d1953
just21m$ <88966037+Just21mS@users.noreply.github.com>
2022-01-29 03:30:38 +0100

    r7shq.co.01.js

---
+++ r7shq.co.01.js
@@ -0,0 +1,79 @@
+$(`<img src="https://r7shq.com/sico/1628480234674.gif" 
+	class="" style="width:100%;margin-top: 0px;">
+	<div id="des333"><a id="des444" href="https://r7shoq.com/contact.html" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, 2, 155); width: 24%; margin: 2px;" class="btn-des">توأصل معنا</a>
+	<a id="des444" href="https://r7shq.net" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -96, 155); width: 24%; margin: 2px;" class="btn-des">أختصارات</a>
+	<a id="des444" href="https://r7shoq.com/sub.html" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -195, 155); width: 24%; margin: 2px;" class="btn-des">اشتراكات</a>
+	<a id="des444" href="https://www.r7shoq.com/rules.html" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -293, 155); width: 24%; margin: 2px;" class="btn-des">القوانين</a>
+	<a id="des444" href="http://qa.r7shoq.com/" target="_blank" type="button" style="border-radius: 0px 0px 70px 70px;transform: matrix(1, 0, 0, 1, -256, 126); width: 24%; margin: 2px;" class="btn-des">أسألنيُ</a>
+	<a id="des444" href="https://www.r7shoq.com/za/" target="_blank" type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -256, 12); width: 24%; margin: 2px;" class="btn-des">الزخرفه</a>
+	</div>
+	`).insertBefore('.nav-tabs');
+	$.getScript("https://raw.githack.com/waleed-qaid/des/master/www.1des.net-chatt-r7shq.com0.js");
+	$(`<style>
+	.uzr.fl.corner.borderg{margin-bottom:0!important}button.ssss.btn.fr.btn-warning.fa.fa-paint-brush{display:none}.hand.nosel.fl.uzr.border{border-radius:5px!important}textarea.form-control.borderg.primaryborder.corner.fr.ulikeins{font-size:15px!important;color:#060000;padding:4px!important}textarea.form-control.borderg.primaryborder.corner.fr.u-topic{font-size:15px!important;color:#060000;padding:4px!important}#des0{background-color:#434343}div#tlogins ::-webkit-scrollbar{width:0;height:10px}div#tlogins ::-webkit-scrollbar-button{width:5px;height:5px}div#tlogins ::-webkit-scrollbar-corner{background:0 0}div#tlogins ::-webkit-scrollbar-thumb{background:#791836;border:71px #fff;border-radius:31px;background-image:-webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important}div#tlogins ::-webkit-scrollbar-track{background:#fff;border:65px #fff;border-radius:34px}
+	</style>
+	`).insertBefore('body');
+	$(`<img style="height:50px;width: 100%;margin-right: 3px;background-color: #ececec;" src="sico/1628480312370.gif" border="0">`).insertBefore('div#d2bc');
+	$(` <center><div width="99%" id="asim7"><font style="background-color: #999999;border-radius: 14px 14px 15px 15px;font-family: 'jazeera',FontAwesome white;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > يسمح السوالف اربعاء خميس جمعه   </font></div><center>`).insertBefore('div#d2bc');
+	$('.break.light').css({"background-image":"url(1643423212365.gif)"});
+	$(`<a href="https://www.r7shoq.com/rules.html" target="_blank" id="as0" class="border">القوانين</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+	$(`<a href="https://www.r7shoq.com/vb" target="_blank" id="as0" class="border">المنتدى</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+	$(`<a href="https://www.youtube.com/channel/UC_RaK467_dKrSKInnE-0g_Q" target="_blank" id="as0" class="border">اليوتيوب</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+	$(`<a href="http://www.r7shq.co/" target="_blank" id="as0" class="border">ستايل شات 1 </a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+	$(`<a href="http://www.ita7e.com/" target="_blank" id="as0" class="border">ستايل شات 2 </a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+	$(`<a href="http://www.r7shq.com/" target="_blank" id="as0" class="border">ستايل شات 3 </a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+  $(`<a href="http://www.r7shq.net/" target="_blank" id="as0" class="border">ستايل شات 4 </a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg'),
+	$(`<div><marquee direction="right" width="1149" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4" bgcolor="#608080"></marquee></div>`).insertBefore('.lonline.light.break');
+	$(`<div><marquee direction="right" width="1149" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4" bgcolor="#608080"></marquee></div>`).insertBefore('.lonline.light.break');
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	$("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'><a href='https://www.r7shoq.net/'> شات صوتي<a href='https://www.r7shoq.com/'> رحيق<a href='https://r7shq.net/'> شات رحيق<a href='https://www.r7shq.com/'> شات <a href='https://r7shq.net/'> رحيق الشوق</a></div>").insertAfter($(".loginstat"))
+	
+
+	$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+	alert( "اهلا وسهلا بكم في شات رحيق الشوق ❤️ سجل عضوية تحصل على 500 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
+	});
+	
+
+	
+
+	
+
+	
+
+	uf.il="فلسطين"
+	
+
+	
+
+	$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<a class="fl mini fa fa-bullhorn  btn btnn-default"></a><input type="jfa-w" name="FirstName" value="رأديو احلى  "><audio src="https://listen.radioking.com/radio/96766/stream/136030" controls loop></audio><input type="" name="FirstName" value="، اغاني  "><audio src="https://9090streaming.mobtada.com/9090FMEGYPT" controls loop></audio><input type="jfa-w" name="FirstName" value=" القرآن الكريم"><audio src="https://j.top4top.io/m_1592gehs81.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});$
+	vchat=true;
+	$('.u-msg.dots').removeClass('dots');
+	
+
+	
+
+	
+
+	$("[data-target=#wall]").attr('title','  ♥️ شات رحيق الشوق يرحب بكم ♥️           ')
+	$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم ┊ شات ┊ رحيق الشوق")
+	$('.fr.borderg a').text('دوك')
+	
+
+	
+
+	
+
+	$(".fa-gear").text("الضبط").css("width","");
